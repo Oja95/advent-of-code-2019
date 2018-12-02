99f72f225847adfa724e36937c491d575f636d28
Tiit Oja <tiit.oja@roguewave.com>
2018-12-02 16:26:00 +0200

    Add test and solution for day 2 part 2

--- src/test/java/ee/aoc/day2/Day2Test.java
+++ src/test/java/ee/aoc/day2/Day2Test.java
@@ -14,4 +14,11 @@ class Day2Test {
     assertEquals(Part1.getChecksum(List.of("abcdef", "bababc", "abbcde", "abcccd", "aabcdd", "abcdee" ,"ababab")), 12);
     assertEquals(Part1.getChecksum(InputFileReaderUtil.getLinesFromClassPathResourceFile("/day2/input.txt")), 6944);
   }
+
+  @Test
+  void testGetCorrectBoxIdIntersection() {
+    assertEquals(Part2.getCorrectBoxesCommonLetters(List.of("abcde", "fghij", "klmno", "pqrst", "fguij", "axcye" ,"wvxyz")), "fgij");
+    assertEquals(Part2.getCorrectBoxesCommonLetters(InputFileReaderUtil.getLinesFromClassPathResourceFile("/day2/input.txt")),
+        "srijafjzloguvlntqmphenbkd");
+  }
 }
