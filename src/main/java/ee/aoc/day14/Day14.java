2c0829398644dc0fe21c1c18eb5c0f3edf3dee8b
Tiit Oja <tiit.oja@roguewave.com>
2018-12-14 23:36:15 +0200

    Add solutions for day 14

--- src/main/java/ee/aoc/day14/Day14.java
+++ src/main/java/ee/aoc/day14/Day14.java
@@ -12,23 +12,35 @@ public class Day14 {
 
   private static void getLastTenRecipes(int lastRecipe) {
     List<Integer> state = new ArrayList<>(List.of(3, 7));
+    List<Integer> lastDigits = digits(lastRecipe);
+    
     int firstElfIndex = 0;
     int secondElfIndex = 1;
     
-    while (state.size() - 10 < lastRecipe) {
+    while (true) {
       Integer firstElfRecipeScore = state.get(firstElfIndex);
       Integer secondElfRecipeScore = state.get(secondElfIndex);
       var newRecipes = digits(firstElfRecipeScore + secondElfRecipeScore);
       state.addAll(newRecipes);
-      
-      int firstElfMoves = 1 + state.get(firstElfIndex);
-      int secondElfMoves = 1 + state.get(secondElfIndex);
-      
-      firstElfIndex = (firstElfIndex + firstElfMoves) % state.size();
-      secondElfIndex = (secondElfIndex + secondElfMoves) % state.size();
+
+      firstElfIndex = (firstElfIndex + 1 + state.get(firstElfIndex)) % state.size();
+      secondElfIndex = (secondElfIndex + 1 + state.get(secondElfIndex)) % state.size();
+
+      // two elves can at most create 2 new recipes on each iteration, so check the end for the pattern or one off the end
+      if (state.size() <= lastDigits.size()) continue;
+      List<Integer> firstPattern = state.subList(state.size() - lastDigits.size(), state.size());
+      List<Integer> secondPattern = state.subList(state.size() - 1 - lastDigits.size(), state.size() - 1);
+      if (lastDigits.equals(firstPattern)) {
+        System.out.println("Part 2: " + (state.size() - lastDigits.size()));
+        break;
+      } else if (lastDigits.equals(secondPattern)) {
+        System.out.println("Part 2: " + (state.size() - 1 - lastDigits.size()));
+        break;
+      }
     }
     
-    System.out.println(state.subList(lastRecipe, lastRecipe + 10).stream().map(String::valueOf).collect(Collectors.joining("")));
+    // Part 1
+    System.out.println("Part 1: " + state.subList(lastRecipe, lastRecipe + 10).stream().map(String::valueOf).collect(Collectors.joining("")));
   }
   
   private static List<Integer> digits(int num) {
